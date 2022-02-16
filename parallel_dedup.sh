814eded0edb78edd30e8898fb0273bcf64a29530
corwinofamber <andkov18@yandex.ru>
2022-02-16 17:05:10 +0300

    Update parallel_dedup.sh

--- parallel_dedup.sh
+++ parallel_dedup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-deduplicate_bismark  --bam  --paired  -o s_8_cell  SRR5836473_1_bismark_bt2_pe.bam > dedup_8_cell.log &
-deduplicate_bismark  --bam  --paired  -o s_epiblast  SRR3824222_1_bismark_bt2_pe.bam > dedup_epiblast.log &
-deduplicate_bismark  --bam  --paired  -o s_icm  SRR5836475_1_bismark_bt2_pe.bam > dedup_icm.log &
+deduplicate_bismark  --bam  --paired  -o s_8_cell  SRR5836473_1_bismark_bt2_pe.bam 2> dedup_8_cell.log &
+deduplicate_bismark  --bam  --paired  -o s_epiblast  SRR3824222_1_bismark_bt2_pe.bam 2> dedup_epiblast.log &
+deduplicate_bismark  --bam  --paired  -o s_icm  SRR5836475_1_bismark_bt2_pe.bam 2> dedup_icm.log &
 wait
